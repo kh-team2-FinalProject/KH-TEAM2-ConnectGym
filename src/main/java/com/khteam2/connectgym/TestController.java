2a0103518b655413829b5b5c896047938827dca7
seohee <seohee905@gmail.com>
2023-08-17 20:04:50 +0900

    이서희 - 마이페이지 추가 / 마이페이지 내 나의수강현황(이름 미정) 페이지 추가

--- src/main/java/com/khteam2/connectgym/TestController.java
+++ src/main/java/com/khteam2/connectgym/TestController.java
@@ -6,29 +6,41 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class TestController {
 
-
+    //메인
     @GetMapping("/")
-    public String index(){
+    public String index() {
         return "content/main";
     }
 
+    //레슨 페이지 내 메뉴 이동
     @GetMapping("/lesson")
-    public String lesson(){
+    public String lesson() {
         return "content/lessonlist";
     }
 
     @GetMapping("/lesson/health")
-    public String lessonHealth(){
+    public String lessonHealth() {
         return "lesson/health";
     }
 
     @GetMapping("/lesson/pilates")
-    public String lessonPilates(){
+    public String lessonPilates() {
         return "lesson/pilates";
     }
 
     @GetMapping("/lesson/yoga")
-    public String lessonYoga(){
+    public String lessonYoga() {
         return "lesson/yoga";
+    }
+
+    //마이페이지 내 메뉴 이동
+    @GetMapping("/mypage")
+    public String myPage() {
+        return "content/mypages";
+    }
+
+    @GetMapping("/mypage/mylesson")
+    public String myLesson() {
+        return "mypage/mylesson";
     }
 }
