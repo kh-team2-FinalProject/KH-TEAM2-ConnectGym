8eab33cf6a3549ddba7fc41518902f5e05155535
JS Lee <slj7121@gmail.com>
2023-08-20 01:29:58 +0900

    이준성 - SessionConstant 클래스 추가
    세션에서 값들을 넣고 꺼낼 때 사용되는 키들을 모아두는 상수 클래스 추가

---
+++ src/main/java/com/khteam2/connectgym/common/SessionConstant.java
@@ -0,0 +1,8 @@
+package com.khteam2.connectgym.common;
+
+public class SessionConstant {
+    /**
+     * 로그인되어 있는 회원 no
+     */
+    public static final String LOGIN_MEMBER_NO = "session_login_member_no";
+}
