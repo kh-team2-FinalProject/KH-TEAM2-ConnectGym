d9a33147c9c7c6e73d67acda4b20667e169767dd
JS Lee <slj7121@gmail.com>
2023-08-20 12:20:20 +0900

    이준성 - Order 컬럼 추가

--- src/main/java/com/khteam2/connectgym/order/Order.java
+++ src/main/java/com/khteam2/connectgym/order/Order.java
@@ -2,16 +2,26 @@ package com.khteam2.connectgym.order;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
 
 import javax.persistence.*;
+import java.sql.Timestamp;
 
-@Entity @Getter
+@Entity
+@Getter
 @NoArgsConstructor
 @Table(name = "orders")
 public class Order {
-
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long no;
-
-
+    @Column(name = "user_no", nullable = false)
+    private Long userNo;
+    @Column(nullable = false, length = 50)
+    private String type;
+    @Column(name = "day_of_payment", nullable = false)
+    @Comment("주문한 날짜 및 시간")
+    private Timestamp dayOfPayment;
+    @Column(name = "order_pay", nullable = false)
+    private Long orderPay;
 }
