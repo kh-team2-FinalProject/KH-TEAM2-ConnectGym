567b896cbef5d46403ea673c39b2c164fd13061f
JS Lee <slj7121@gmail.com>
2023-08-22 12:18:58 +0900

    이준성 - 주문 관련된 DTO 생성

---
+++ src/main/java/com/khteam2/connectgym/order/dto/OrderProcessDto.java
@@ -0,0 +1,18 @@
+package com.khteam2.connectgym.order.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+@Builder
+public class OrderProcessDto {
+    private String imp_uid;
+    private String merchant_uid;
+    private Boolean imp_success;
+    private String error_code;
+    private String error_msg;
+}
