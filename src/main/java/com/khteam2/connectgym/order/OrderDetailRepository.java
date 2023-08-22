de013e5d5cd56f697c6e8142f0459970fa546516
JS Lee <slj7121@gmail.com>
2023-08-22 12:18:36 +0900

    이준성 - OrderDetailRepository 추가

---
+++ src/main/java/com/khteam2/connectgym/order/OrderDetailRepository.java
@@ -0,0 +1,9 @@
+package com.khteam2.connectgym.order;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderDetailRepository extends JpaRepository<OrderDetail, Long> {
+    List<OrderDetail> findByOrder(Order order);
+}
