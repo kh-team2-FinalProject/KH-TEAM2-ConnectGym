b853a4e0cced02b7691e50b4d796848ff8391e47
JS Lee <slj7121@gmail.com>
2023-08-24 16:58:45 +0900

    이준성 - 문제 수정 및 변수 추가
    - 클래스가 정상적으로 작동하지 않는 문제 해결
    - 인터셉터를 의존성 주입 방식으로 변경

--- src/main/java/com/khteam2/connectgym/common/WebMvcConfig.java
+++ src/main/java/com/khteam2/connectgym/common/WebMvcConfig.java
@@ -1,15 +1,19 @@
 package com.khteam2.connectgym.common;
 
 import com.khteam2.connectgym.member.interceptor.LoginCheckInterceptor;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@EnableWebMvc
+@Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+    @Autowired
+    private LoginCheckInterceptor loginCheckInterceptor;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new LoginCheckInterceptor())
+        registry.addInterceptor(this.loginCheckInterceptor)
             .order(1)
             // 아래 지정한 URL에 한해서 로그인 유무를 확인한다.
             .addPathPatterns("/order/**");
