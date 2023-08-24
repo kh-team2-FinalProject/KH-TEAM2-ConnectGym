671933b05f9c14a468932897ed45430848468e34
JS Lee <slj7121@gmail.com>
2023-08-24 09:14:33 +0900

    이준성 - findByMember 메소드 추가

--- src/main/java/com/khteam2/connectgym/order/OrderRepository.java
+++ src/main/java/com/khteam2/connectgym/order/OrderRepository.java
@@ -1,8 +1,13 @@
 package com.khteam2.connectgym.order;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.khteam2.connectgym.member.Member;
+
 public interface OrderRepository extends JpaRepository<Order, String> {
+    List<Order> findByMember(Member member);
     // @Query(value = "SELECT * FROM orders o" +
     //     " JOIN order_details od ON od.order_no = o.no" +
     //     " JOIN lesson l ON l.id = od.lesson_no"
