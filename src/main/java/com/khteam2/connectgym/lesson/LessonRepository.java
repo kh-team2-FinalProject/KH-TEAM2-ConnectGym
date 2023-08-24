a27f98d93fa1345ca7f860f985f164f2e08c642c
JS Lee <slj7121@gmail.com>
2023-08-24 09:15:38 +0900

    이준성 - 멤버 번호로 레슨 찾는 메소드 추가

--- src/main/java/com/khteam2/connectgym/lesson/LessonRepository.java
+++ src/main/java/com/khteam2/connectgym/lesson/LessonRepository.java
@@ -1,12 +1,15 @@
 package com.khteam2.connectgym.lesson;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 public interface LessonRepository extends JpaRepository<Lesson, Long> {
-
-
-
+    @Query(value = "SELECT l.* FROM users u" + " JOIN orders o ON o.member_no = u.`no`"
+            + " JOIN order_detail od ON od.order_no = o.`no`"
+            + " JOIN lessons l ON l.`no` = od.lesson_no"
+            + " WHERE u.`no` = :memberId", nativeQuery = true)
+    List<Lesson> findByMemberNo(@Param("memberId") Long memberNo);
 }
