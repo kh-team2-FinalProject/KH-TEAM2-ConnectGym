88389a6395bf2d1bc339224f319e4b2c57ac69cc
JS Lee <slj7121@gmail.com>
2023-08-28 12:55:00 +0900

    이준성 - 유틸 클래스 추가

---
+++ src/main/java/com/khteam2/connectgym/common/CommonUtil.java
@@ -0,0 +1,37 @@
+package com.khteam2.connectgym.common;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
+
+public class CommonUtil {
+    private static final Random random = new Random();
+
+    /**
+     * 오늘 날짜를 반환하는 메소드
+     * @return yyyyMMdd 형식의 날짜 (예: {@code "20230828"})
+     */
+    public static String getTodayLocalDate8() {
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+    }
+
+    /**
+     * 1 ~ (9...) 난수 생성기
+     * @param digit 1부터 18 사이의 자릿수
+     * @return 파라미터로 넘긴 자릿수의 값에 따라 달라짐
+     * (예: 파라미터 값으로 {@code 3}을 넘겼다면 1~999 사이의 난수를 반환함)
+     */
+    public static long generateRandomNumberLong(int digit) {
+        if (digit <= 0 || digit > 18) {
+            return -1;
+        }
+
+        StringBuilder digitSb = new StringBuilder();
+        for (int i = 0; i < digit; i++) {
+            digitSb.append("9");
+        }
+        long val = Long.parseLong(digitSb.toString());
+
+        return random.nextLong(val) + 1;
+    }
+}
