b02927db4fe5250be645a4bd7dd04fbf2ca7bc08
JS Lee <slj7121@gmail.com>
2023-08-28 15:21:11 +0900

    이준성 - CommonUtil 클래스 수정
    - random 타입을 ThreadLocalRandom으로 변경
    - 로직 일부 개선
    - generateRandomNumberLong 메소드 추가

--- src/main/java/com/khteam2/connectgym/common/CommonUtil.java
+++ src/main/java/com/khteam2/connectgym/common/CommonUtil.java
@@ -2,13 +2,14 @@ package com.khteam2.connectgym.common;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class CommonUtil {
-    private static final Random random = new Random();
+    private static final ThreadLocalRandom random = ThreadLocalRandom.current();
 
     /**
      * 오늘 날짜를 반환하는 메소드
+     *
      * @return yyyyMMdd 형식의 날짜 (예: {@code "20230828"})
      */
     public static String getTodayLocalDate8() {
@@ -17,21 +18,49 @@ public class CommonUtil {
 
     /**
      * 1 ~ (9...) 난수 생성기 (최대 999,999,999)
+     *
      * @param digit 1부터 9 사이의 자릿수
      * @return 파라미터로 넘긴 자릿수의 값에 따라 달라짐
-     * (예: 파라미터 값으로 {@code 3}을 넘겼다면 1~999 사이의 난수를 반환함)
+     * (예: 파라미터 값으로 {@code 3}을 넘겼다면 1~999 사이의 난수를 반환함).
+     * 유효하지 않은 digit을 넘길 경우 음수 반환
      */
     public static int generateRandomNumberInt(int digit) {
-        if (digit <= 0 || digit > 9) {
+        if (digit < 1 || digit > 9) {
             return -1;
         }
 
-        StringBuilder digitSb = new StringBuilder();
-        for (int i = 0; i < digit; i++) {
-            digitSb.append("9");
-        }
-        int val = Integer.parseInt(digitSb.toString());
+        String boundStr = randomNumberGetBoundString(digit);
+        int bound = Integer.parseInt(boundStr);
 
-        return random.nextInt(val) + 1;
+        return random.nextInt(bound) + 1;
+    }
+
+    /**
+     * 1 ~ (9...) 난수 생성기 (최대 999,999,999,999,999,999)
+     *
+     * @param digit 1부터 18 사이의 자릿수
+     * @return 파라미터로 넘긴 자릿수의 값에 따라 달라짐
+     * (예: 파라미터 값으로 {@code 3}을 넘겼다면 1~999 사이의 난수를 반환함).
+     * 유효하지 않은 digit을 넘길 경우 음수 반환
+     */
+    public static long generateRandomNumberLong(int digit) {
+        if (digit < 1 || digit > 18) {
+            return -1L;
+        }
+
+        String boundStr = randomNumberGetBoundString(digit);
+        long bound = Long.parseLong(boundStr);
+
+        return random.nextLong(bound) + 1L;
+    }
+
+    private static String randomNumberGetBoundString(int digit) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < digit; i++) {
+            sb.append("9");
+        }
+
+        return sb.toString();
     }
 }
