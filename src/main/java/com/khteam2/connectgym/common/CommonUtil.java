4cd8dd6515cbf8f22c7a7a1050ee026364f0e607
JS Lee <slj7121@gmail.com>
2023-08-28 13:16:27 +0900

    이준성 - 난수 생성기 오류 수정
    - nextLong 메소드를 찾을 수 없어 int로 대체

--- src/main/java/com/khteam2/connectgym/common/CommonUtil.java
+++ src/main/java/com/khteam2/connectgym/common/CommonUtil.java
@@ -16,13 +16,13 @@ public class CommonUtil {
     }
 
     /**
-     * 1 ~ (9...) 난수 생성기
-     * @param digit 1부터 18 사이의 자릿수
+     * 1 ~ (9...) 난수 생성기 (최대 999,999,999)
+     * @param digit 1부터 9 사이의 자릿수
      * @return 파라미터로 넘긴 자릿수의 값에 따라 달라짐
      * (예: 파라미터 값으로 {@code 3}을 넘겼다면 1~999 사이의 난수를 반환함)
      */
-    public static long generateRandomNumberLong(int digit) {
-        if (digit <= 0 || digit > 18) {
+    public static int generateRandomNumberInt(int digit) {
+        if (digit <= 0 || digit > 9) {
             return -1;
         }
 
@@ -30,8 +30,8 @@ public class CommonUtil {
         for (int i = 0; i < digit; i++) {
             digitSb.append("9");
         }
-        long val = Long.parseLong(digitSb.toString());
+        int val = Integer.parseInt(digitSb.toString());
 
-        return random.nextLong(val) + 1;
+        return random.nextInt(val) + 1;
     }
 }
