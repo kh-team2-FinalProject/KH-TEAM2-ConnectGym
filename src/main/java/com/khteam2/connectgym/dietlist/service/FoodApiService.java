451c0f0e57a82325a159bb4c4193e1db29e4e896
imbojoeng <qhwjd8447@naver.com>
2023-08-29 17:50:28 +0900

    임보정 - 사용할 db 변경, 작업 중

--- src/main/java/com/khteam2/connectgym/dietlist/service/FoodApiService.java
+++ src/main/java/com/khteam2/connectgym/dietlist/service/FoodApiService.java
@@ -4,6 +4,13 @@ import com.khteam2.connectgym.dietlist.model.Food;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+
+
+
+/*
+* API NEW
+* http://openapi.foodsafetykorea.go.kr/api/2850a192ad63430f8bbf/I2790/xml/1/5
+* */
 @Service
 public class FoodApiService {
     private final String apiKey = "zYPK1Bj9cpL%2FZ1nD8%2Fr56or2XJaCFvizZqM9ZQ4oxjDhjtfMHceoEtq4%2BrwcNJoynkMj5DWZk9EIxH8bwPzs8Q%3D%3D";
