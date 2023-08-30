b045f326ec3077157dc1710d547d7c45d9c94b62
김우영 <>
2023-08-30 14:12:41 +0900

    김우영	 채팅방 엔티티

--- src/main/java/com/khteam2/connectgym/chat_test/ChatMessage.java
+++ src/main/java/com/khteam2/connectgym/chat_test/ChatMessage.java
@@ -1,17 +1,39 @@
 package com.khteam2.connectgym.chat_test;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import java.time.LocalDateTime;
 
 @Data
+@Entity
 public class ChatMessage {
 
+    @Id
+    @GeneratedValue
+    private Long no;
+
+
+    private Long chattingRoomNo;
 
     private String content;
     private String sender;
+    private LocalDateTime timestamp;
+
+    public ChatMessage(Long no, Long chattingRoomNo, String content, String sender, LocalDateTime timestamp) {
+        this.no = no;
+        this.chattingRoomNo = chattingRoomNo;
+        this.content = content;
+        this.sender = sender;
+        this.timestamp = timestamp;
+    }
 
     public ChatMessage() {
     }
 
+    //메시지 테스트용 생성자
     public ChatMessage(String content, String sender) {
         this.content = content;
         this.sender = sender;
