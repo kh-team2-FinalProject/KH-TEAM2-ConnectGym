08f8ce09f03c9920617902f9721a593377e8aa2b
JS Lee <slj7121@gmail.com>
2023-08-30 16:06:10 +0900

    이준성 - 트레이너 로그인 서비스 메소드 추가

--- src/main/java/com/khteam2/connectgym/trainer/TrainerService.java
+++ src/main/java/com/khteam2/connectgym/trainer/TrainerService.java
@@ -2,20 +2,18 @@ package com.khteam2.connectgym.trainer;
 
 
 import com.khteam2.connectgym.member.Member;
+import com.khteam2.connectgym.member.MemberClass;
 import com.khteam2.connectgym.member.MemberRepository;
+import com.khteam2.connectgym.member.dto.MemberLoginRequestDto;
+import com.khteam2.connectgym.member.dto.MemberLoginResponseDto;
 import com.khteam2.connectgym.trainer.dto.TrainerRequestDTO;
 import com.khteam2.connectgym.upload.S3Uploader;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -28,12 +26,12 @@ public class TrainerService {
 
     @Transactional
     public Long registerTrainer(TrainerRequestDTO trainerRequestDTO, Member member,
-                                  MultipartFile profileImgFile,MultipartFile[] licenseImgFiles) {
+                                MultipartFile profileImgFile, MultipartFile[] licenseImgFiles) {
         String fileUrl = "";
         //프로필사진
         if (!profileImgFile.isEmpty()) {
             try {
-                fileUrl = s3Uploader.uploadProfileFile(profileImgFile,member.getUserId());
+                fileUrl = s3Uploader.uploadProfileFile(profileImgFile, member.getUserId());
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
@@ -44,7 +42,7 @@ public class TrainerService {
             Licenses licenses = new Licenses();
 
             try {
-                for(MultipartFile file: licenseImgFiles){
+                for (MultipartFile file : licenseImgFiles) {
                     licenses.setLicenseImg(s3Uploader.uploadProfileFile(file, member.getUserId()));
                     trainerRequestDTO.addLicense(licenses);
                 }
@@ -56,16 +54,48 @@ public class TrainerService {
 
 
         TrainerRequestDTO dto = TrainerRequestDTO.builder()
-                                .trainerId(member.getUserId())
-                                .trainerPw(member.getUserPw())
-                                .trainerName(member.getUserName())
-                                .trainerTel(member.getUserTel())
-                                .profileImg(fileUrl)
-                                .trainerInfo(trainerRequestDTO.getTrainerInfo())
-                                .build();
+            .trainerId(member.getUserId())
+            .trainerPw(member.getUserPw())
+            .trainerName(member.getUserName())
+            .trainerTel(member.getUserTel())
+            .profileImg(fileUrl)
+            .trainerInfo(trainerRequestDTO.getTrainerInfo())
+            .build();
 
         Long trainerNo = trainerRepository.save(dto.toEntity()).getNo();
 
         return trainerNo;
+    }
+
+    public MemberLoginResponseDto trainerLogin(MemberLoginRequestDto requestDto) {
+        MemberLoginResponseDto responseDto = MemberLoginResponseDto.builder()
+            .success(false)
+            .build();
+
+        if (requestDto == null) {
+            responseDto.setMessage("잘못된 요청입니다.");
+            return responseDto;
+        }
+
+        String id = requestDto.getId();
+        String password = requestDto.getPassword();
+
+        if (id == null || id.isBlank() || password == null || password.isBlank()) {
+            responseDto.setMessage("ID 또는 비밀번호가 비어있습니다.");
+            return responseDto;
+        }
+
+        Member member = this.memberRepository.findByUserId(id);
+
+        if (member == null || !member.getUserPw().equals(password)) {
+            responseDto.setMessage("ID 또는 비밀번호를 확인해 주시기 바랍니다.");
+            return responseDto;
+        }
+
+        responseDto.setSuccess(true);
+        responseDto.setMemberNo(member.getNo());
+        responseDto.setMemberClass(MemberClass.TRAINER);
+
+        return responseDto;
     }
 }
