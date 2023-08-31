987df663e2f4f77827ce9b004e69fb59689eb41e
JS Lee <slj7121@gmail.com>
2023-08-31 21:17:57 +0900

    이준성 - 회사 이름으로 검색하는 메소드 추가

--- src/main/java/com/khteam2/connectgym/dietlist/repository/FoodRepository.java
+++ src/main/java/com/khteam2/connectgym/dietlist/repository/FoodRepository.java
@@ -1,9 +1,13 @@
 package com.khteam2.connectgym.dietlist.repository;
 
-import com.khteam2.connectgym.dietlist.model.Food;
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.khteam2.connectgym.dietlist.model.Food;
+
 public interface FoodRepository extends JpaRepository<Food, Long> {
-//    List<Food> findByAnimal_PlantContains(String animal_plant);
+    List<Food> findByAnimalPlantContains(String animalPlant, Pageable pageable);
 }
 // JpaRepository를 통해 기본적인 CRUD(Create, Read, Update, Delete) 메소드가 제공됨
