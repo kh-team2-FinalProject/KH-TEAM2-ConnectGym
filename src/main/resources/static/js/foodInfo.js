c84f04ce93c7b56669ac1db9a95f08ec1cc3b83d
Imbojeong <qhwjd8447@naver.com>
2023-09-04 14:28:01 +0900

    임보정 - 유효성, 상품등록 swal 적용 , (어제 controller 로 유효성하다가 메세지 창이 자꾸 이상하게 떠서 js로 했습니다)

--- src/main/resources/static/js/foodInfo.js
+++ src/main/resources/static/js/foodInfo.js
@@ -6,28 +6,6 @@ function openPop() {
     modal.style.display= "block";
 }
 
-document.getElementById("submit").addEventListener("click", function () {
-    // 입력 필드 값 가져오기
-    var foodNm = document.querySelector("#foodNm").value;
-    var foodSize = document.querySelector("#foodSize").value;
-    var choc = document.querySelector("#choc").value;
-    var prot = document.querySelector("#prot").value;
-    var fat = document.querySelector("#fat").value;
-
-    e.preventDefault();
-
-if (!foodNm || !foodSize || !choc || !prot || !fat) {
-    Swal.fire({
-      icon: 'error',
-      title: '필수 입력 항목이 비었습니다.',
-      confirmButtonColor: '#3085d6'
-    })
-    return;
-} else {
-    document.querySelector("form").submit();
-    }
-});
-
 
 /* 버튼 클릭시 닫힘 */
 document.getElementById("closeBtn").addEventListener("click", function() {
@@ -38,10 +16,12 @@ document.getElementById("closeBtn").addEventListener("click", function() {
 
 
 
+
 /*
- 유효성
 document.getElementById("submit").addEventListener("click", function(e) {
         e.preventDefault(); // 기본 이벤트 (폼 제출) 방지
+
+
 
         // 입력 필드 값 가져오기
         var foodNm = document.querySelector("#foodNm").value;
@@ -52,7 +32,13 @@ document.getElementById("submit").addEventListener("click", function(e) {
 
         // 필수 입력 필드가 비어 있는지 확인
         if (!foodNm || !foodSize || !choc || !prot || !fat) {
-            alert("필수 입력 필드를 모두 작성하세요.");
+            */
+/*alert("필수 입력 필드를 모두 작성하세요.");*//*
+
+            Swal.fire({
+                  icon: 'error',
+                  title: '필수 입력 항목이 비었습니다.'
+                })
             return;
         }
 
@@ -80,6 +66,3 @@ $(document).ready(function(){
     })
 })
 
-<script src="sweetalert2.all.min.js"></script>
-<script src="sweetalert2.min.js"></script>
-<link rel="stylesheet" href="sweetalert2.min.css">
