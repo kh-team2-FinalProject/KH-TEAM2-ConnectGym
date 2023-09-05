13853fbf245a70bac49cf9ebbf2e31ff90e1e9bd
김우영 <>
2023-09-05 19:08:19 +0900

    김우영 말풍선 구현 스크롤바 하단고정

--- src/main/java/com/khteam2/connectgym/chat_test/ChatController.java
+++ src/main/java/com/khteam2/connectgym/chat_test/ChatController.java
@@ -2,6 +2,8 @@ package com.khteam2.connectgym.chat_test;
 
 import com.khteam2.connectgym.chat_test.dto.ChatMessageDTO;
 import com.khteam2.connectgym.chat_test.dto.ChatMessageReaponseDTO;
+import com.khteam2.connectgym.common.SessionConstant;
+import com.khteam2.connectgym.member.MemberClass;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -23,7 +25,8 @@ public class ChatController {
 
     @GetMapping("/chat_test/{chatroomNo}")
     public String chat_open(Model model,
-                            @PathVariable("chatroomNo") Long chatroomNo) {
+                            @PathVariable("chatroomNo") Long chatroomNo,
+                            @SessionAttribute(name = SessionConstant.LOGIN_MEMBER_CLASS, required = false) MemberClass memberClass) {
         Chatroom chatroom = chatroomService.inChatroom(chatroomNo);
 //        ChatroomDTO chatroomDTO = new ChatroomDTO().fromEntity(chatroom);
         System.out.println("chatroomNo=====================");
@@ -34,7 +37,14 @@ public class ChatController {
         model.addAttribute("chatroomNo", chatroomNo);
 //        model.addAttribute("chatroomDTO", chatroomDTO);
         model.addAttribute("chatroom", chatroom);
+        String sender = "";
 
+        if (memberClass == MemberClass.MEMBER) {
+            sender = chatroom.getMember().getUserName();
+        } else {
+            sender = chatroom.getTrainer().getTrainerName();
+        }
+        model.addAttribute("sender", sender);
 
         return "chat_test/chat_test2";
     }
@@ -61,8 +71,7 @@ public class ChatController {
     public void sendMessage(@DestinationVariable Long chatroomNo,
                             @Payload ChatMessageDTO message) {
 
-        System.out.println("chatroomNo = " + chatroomNo);
-        System.out.println("message = " + message);
+
         ChatMessage chatMessage = chatMessageService.saveMessage(chatroomNo, message);
         ChatMessageReaponseDTO chatMessageReaponseDTO = new ChatMessageReaponseDTO().fromEntity(chatMessage);
 
--- src/main/resources/static/js/chatroom.js
+++ src/main/resources/static/js/chatroom.js
@@ -1,6 +1,5 @@
 function checkedChatroom() {
-
-    //아작스 통신으로 채팅룸 검사 함수가 있는 컨트롤러로 보낸후 리턴값으로 채팅룸을 반환 chatroomNO
+    //아작스 통신으로 채팅룸 검사 함수가 있는 컨트롤러로 보낸후 리턴값으로 채팅룸을 반환 chatroom
     $.ajax({
         type: "POST",
         url: "/checkedChatroom",
@@ -19,26 +18,87 @@ function checkedChatroom() {
             alert("채팅방 연결에 실패하였습니다.")
         }
     });
+
 }
 
 
 function sendMessage() {
-    
-    var content = document.getElementById('message').value;
-    stompClient.send(`/app/chat/${chatroomNo}`, {}, JSON.stringify({
-        'chatroomNo': chatroomNo,
-        'content': content,
-        'sender': sender
 
-    }));
-    document.getElementById('message').value = '';
+    var content = document.getElementById('message').value;
+    if (content.trim() !== "") {
+//공백이 아니면 STOMP이용해ㅓㅅ 해당주소로 JSON타입으로 메시지 발송
+        stompClient.send(`/app/chat/${chatroomNo}`, {}, JSON.stringify({
+            'chatroomNo': chatroomNo,
+            'content': content,
+            'sender': sender
+
+        }));
+        document.getElementById('message').value = '';
+    }
+    endtoscrollbar();
 }
 
+//내가 보낸 메시지와 다른사람이 보낸 메시지 구별해서 화면에 출력
 function showMessage(message) {
-    var chatBox = document.getElementById('chat-box');
-    var messageBody = document.createElement('div');
-    messageBody.textContent = message.sender + ': ' + message.content; // 보낸 사람 정보 표시
-    chatBox.appendChild(messageBody);
-    // document.querySelector("#messages").appendChild(messageDiv);
 
+    var chatBox = document.getElementById("chat-box");      //채팅이 들어갈 전체영역
+    if (message.sender !== sender) {
+
+        var chatDiv = document.createElement("div");          //채팅메시지 요소들이 묶이는 영역
+        chatDiv.className = "partnerMessageBox chat"; //chat  ch1
+
+        var messageBody = document.createElement("div");       //채팅 메시지
+        messageBody.className = "messageBody";   //textbox
+        messageBody.textContent = message.content;
+
+        // var dateSpan = document.createElement('span');                               //보낸시간
+        // dateSpan.className = 'tx-small';
+        // dateSpan.textContent = date;
+
+        chatDiv.appendChild(messageBody);
+        // chatDiv.appendChild(dateSpan);
+
+        chatBox.appendChild(chatDiv);
+    } else {
+        var chatDiv = document.createElement("div");
+        chatDiv.className = "myMessageBox chat";    //chat  ch2
+
+        var messageBody = document.createElement("div");
+        messageBody.className = "messageBody";     //textbox
+        messageBody.textContent = message.content;
+
+        // var dateSpan = document.createElement('span');
+        // dateSpan.className = 'tx-small';
+        // dateSpan.textContent = date;
+
+        chatDiv.appendChild(messageBody);
+        // chatDiv.appendChild(dateSpan);
+
+        chatBox.appendChild(chatDiv);
+
+    }
+    const scrollTop = chatBox.scrollTop;
+    const scrollHeight = chatBox.scrollHeight;
+    const clientHeight = chatBox.clientHeight;
+    console.log(scrollTop + clientHeight);
+    console.log(scrollHeight);
+
+
+    // 스크롤 위치가 가장 하단에 있을 때만 하단고정
+    if (scrollTop + clientHeight + 100 >= scrollHeight) {
+        endtoscrollbar();
+    }
+}
+
+//스크롤바 하단고정 함수
+function endtoscrollbar() {
+
+    const chatcontainer = document.getElementById("chat-box");
+
+    requestAnimationFrame(() => {
+        chatcontainer.scrollTo(0, chatcontainer.scrollHeight);
+    });
+
+    // chatcontainer.scrollIntoView({block: 'end', behavior: 'smooth'});
+    // }
 }
