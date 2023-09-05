4f62baaec486f07970ccdc27188b87b8ed7a2145
JS Lee <slj7121@gmail.com>
2023-09-05 16:36:32 +0900

    이준성 - Repository 추가

---
+++ src/main/java/com/khteam2/connectgym/order/OrderProgressDetailRepository.java
@@ -0,0 +1,7 @@
+package com.khteam2.connectgym.order;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderProgressDetailRepository extends JpaRepository<OrderProgressDetail, Long> {
+
+}
