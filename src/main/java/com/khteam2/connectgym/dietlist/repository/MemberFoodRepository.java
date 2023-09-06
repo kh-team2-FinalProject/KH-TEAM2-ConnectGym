4aa696d8398f169fa529f96e633ef573e54e5ac0
JS Lee <slj7121@gmail.com>
2023-09-06 19:17:36 +0900

    이준성 - Repository 추가

---
+++ src/main/java/com/khteam2/connectgym/dietlist/repository/MemberFoodRepository.java
@@ -0,0 +1,8 @@
+package com.khteam2.connectgym.dietlist.repository;
+
+import com.khteam2.connectgym.dietlist.model.MemberFood;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberFoodRepository extends JpaRepository<MemberFood, Long> {
+
+}
