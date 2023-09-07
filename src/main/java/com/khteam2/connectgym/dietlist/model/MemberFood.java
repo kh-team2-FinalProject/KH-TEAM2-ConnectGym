cc6043688bb493aaaf3fa6a4fc3630ad800974e7
JS Lee <slj7121@gmail.com>
2023-09-07 12:19:09 +0900

    이준성 - LAZY 옵션 추가

--- src/main/java/com/khteam2/connectgym/dietlist/model/MemberFood.java
+++ src/main/java/com/khteam2/connectgym/dietlist/model/MemberFood.java
@@ -2,23 +2,27 @@ package com.khteam2.connectgym.dietlist.model;
 
 import com.khteam2.connectgym.dietlist.FoodTime;
 import com.khteam2.connectgym.member.Member;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 public class MemberFood {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long no;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_no", nullable = false)
     private Member member;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "food_no", nullable = false)
     private Food food;
     @Column(nullable = false)
