0e3c512fa91019632c180607f17ab10dde4c4b30
JS Lee <slj7121@gmail.com>
2023-09-07 12:19:48 +0900

    이준성 - 멤버와 등록일로 찾는 메소드 추가

--- src/main/java/com/khteam2/connectgym/dietlist/repository/MemberFoodRepository.java
+++ src/main/java/com/khteam2/connectgym/dietlist/repository/MemberFoodRepository.java
@@ -1,8 +1,14 @@
 package com.khteam2.connectgym.dietlist.repository;
 
-import com.khteam2.connectgym.dietlist.model.MemberFood;
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.khteam2.connectgym.dietlist.model.MemberFood;
+import com.khteam2.connectgym.member.Member;
 
 public interface MemberFoodRepository extends JpaRepository<MemberFood, Long> {
-
+    List<MemberFood> findByMemberAndRegDate(@Param("member") Member member, @Param("regDate") LocalDate regDate);
 }
