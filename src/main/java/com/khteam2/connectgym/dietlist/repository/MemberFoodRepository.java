f3d887eaf561a23a03fbd5a42e2b03f385c3b9e3
JS Lee <slj7121@gmail.com>
2023-09-07 14:27:58 +0900

    이준성 - 멤버 및 번호로 찾는 메소드 추가

--- src/main/java/com/khteam2/connectgym/dietlist/repository/MemberFoodRepository.java
+++ src/main/java/com/khteam2/connectgym/dietlist/repository/MemberFoodRepository.java
@@ -1,14 +1,15 @@
 package com.khteam2.connectgym.dietlist.repository;
 
-import java.time.LocalDate;
-import java.util.List;
-
+import com.khteam2.connectgym.dietlist.model.MemberFood;
+import com.khteam2.connectgym.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 
-import com.khteam2.connectgym.dietlist.model.MemberFood;
-import com.khteam2.connectgym.member.Member;
+import java.time.LocalDate;
+import java.util.List;
 
 public interface MemberFoodRepository extends JpaRepository<MemberFood, Long> {
     List<MemberFood> findByMemberAndRegDate(@Param("member") Member member, @Param("regDate") LocalDate regDate);
+
+    MemberFood findByMemberAndNo(Member member, Long no);
 }
