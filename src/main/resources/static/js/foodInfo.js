630da7b71cb813fbf87f57f5b2a73789f53f6934
JS Lee <slj7121@gmail.com>
2023-09-08 15:56:43 +0900

    이준성 - 음식 정보 검색 페이지 수정
    - 코드 정렬
    - 일부 항목 백엔드에 맞게 수정
    - 페이징 추가 및 일부 디자인
    - onload, document.ready 주석 처리
    - movePage() 페이지 이동 함수 추가

--- src/main/resources/static/js/foodInfo.js
+++ src/main/resources/static/js/foodInfo.js
@@ -1,45 +1,43 @@
-
-
 /* 열림 */
 function openPop() {
     var modal = document.getElementById("dietPopup");
-    modal.style.display= "block";
+    modal.style.display = "block";
 }
 
 /* 버튼 클릭시 닫힘 */
-document.getElementById("closeBtn").addEventListener("click", function() {
+document.getElementById("closeBtn").addEventListener("click", function () {
     var modal = document.getElementById("dietPopup");
     modal.style.display = "none";
 });
 
-
-
 /* 검색 */
 function searchFood() {
-        var foodName = document.getElementById("foodNameInput").value;
-        var form = document.getElementById("foodSearchForm");
-        form.action = "/fooddiary/foodinfo?key=" + foodName;
-        form.submit();
-    }
-
-
-
+    var foodName = document.getElementById("foodNameInput").value;
+    var form = document.getElementById("foodSearchForm");
+    form.action = "/fooddiary/foodinfo?search=" + foodName;
+    form.submit();
+}
 
 /* 검색 기능 */
-$(document).ready(function(){
+// $(document).ready(function () {
+//     // Enter
+//     $("#popInput").keypress(function (e) {
+//         if (e.which === 13) {
+//             const searchValue = $("#popInput").val();
+//             if (searchValue.trim() !== "") {
+//                 searchFoods(searchValue);
+//             }
+//         }
+//     });
+// });
 
-    // Enter
-    $("#popInput").keypress(function (e){
-        if(e.which === 13){
-            const searchValue = $("#popInput").val();
-            if(searchValue.trim() !== ""){
-                searchFoods(searchValue);
-            }
-        }
-    })
-})
+// window.onload = function () {
+//     var modal = document.getElementById("dietPopup");
+//     modal.style.display = "none";
+// };
 
-window.onload = function() {
-    var modal = document.getElementById("dietPopup");
-    modal.style.display = "none";
+function movePage(page) {
+    const params = new URLSearchParams(location.search);
+    params.set("page", page);
+    location.href = location.pathname + "?" + params;
 }
